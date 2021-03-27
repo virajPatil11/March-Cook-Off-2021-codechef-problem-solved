c49d987f8382becee40f86ea22bf913d00859197
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-27 08:06:06 +0530

    Add files via upload

---
+++ pawriMeme.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 07:46:30 2021
+
+@author: raj patil
+"""
+
+#test case value
+for i in range(int(input())):
+    #input the string
+    msg = input()
+    #using replace method to change substring
+    x = msg.replace("party", "pawri")
+    print(x)
